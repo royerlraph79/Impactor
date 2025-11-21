f96f56d212324dc4f9037d3eb6e3cb345fe518b4
flower <97859147+khcrysalis@users.noreply.github.com>
2025-11-21 04:18:04 -0800

    feat: icon for windows

--- apps/plumeimpactor/build.rs
+++ apps/plumeimpactor/build.rs
@@ -1,12 +1,9 @@
 #[cfg(windows)]
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    let target = std::env::var("TARGET").unwrap_or_default();
 
-    if target.contains("windows") {
-        let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
-        embed_windows_manifest(&pkg_name);
-    }
+    let pkg_name = std::env::var("CARGO_PKG_NAME").unwrap();
+    embed_windows_manifest(&pkg_name);
 }
 
 #[cfg(not(windows))]
@@ -17,15 +14,20 @@ fn embed_windows_manifest(name: &str) {
     use embed_manifest::manifest::{ActiveCodePage, Setting, SupportedOS::*};
     use embed_manifest::{embed_manifest, new_manifest};
 
-    let manifest = new_manifest(name)
-        .supported_os(Windows7..=Windows10)
-        .active_code_page(ActiveCodePage::Utf8)
-        .heap_type(embed_manifest::manifest::HeapType::SegmentHeap)
-        .dpi_awareness(embed_manifest::manifest::DpiAwareness::PerMonitorV2)
-        .long_path_aware(Setting::Enabled);
+    {
+        let manifest = new_manifest(name)
+            .supported_os(Windows7..=Windows10)
+            .active_code_page(ActiveCodePage::Utf8)
+            .heap_type(embed_manifest::manifest::HeapType::SegmentHeap)
+            .dpi_awareness(embed_manifest::manifest::DpiAwareness::PerMonitorV2)
+            .long_path_aware(Setting::Enabled);
 
-    if let Err(e) = embed_manifest(manifest) {
-        println!("cargo:warning=Failed to embed manifest: {e}");
-        println!("cargo:warning=The application will still work but may lack optimal Windows theming");
+        embed_manifest(manifest).unwrap();
+    }
+
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("../../package/windows/icon.ico");
+        res.compile().unwrap();
     }
 }
